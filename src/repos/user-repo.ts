57cdf115cfe618f731ecd8cdec56f5b848d49a9d
KaneishaR <kaneisha.reese@gmail.com>
2020-05-14 02:56:11 -0500

    setting up api pipeline

--- src/repos/user-repo.ts
+++ src/repos/user-repo.ts
@@ -1,11 +1,17 @@
-import { User } from '../models/user'
-import { CrudRepository } from "./crud-repo";
+import { User } from '../models/user';
+import { CrudRepository } from './crud-repo';
+import {
+    NotImplementedError, 
+    ResourceNotFoundError, 
+    ResourcePersistenceError,
+    InternalServerError
+} from '../errors/errors';
 import { PoolClient } from 'pg';
 import { connectionPool } from '..';
-import { InternalServerError } from '../errors/errors';
-
+import { mapUserResultSet } from '../util/result-set-mapper';
 
 export class UserRepository implements CrudRepository<User> {
+
     baseQuery = `
         select
             au.id, 
@@ -37,19 +43,121 @@ export class UserRepository implements CrudRepository<User> {
     
     }
 
-    async getById(): Promise<User>{
-        return 
+    async getById(id: number): Promise<User> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `${this.baseQuery} where au.id = $1`;
+            let rs = await client.query(sql, [id]);
+            return mapUserResultSet(rs.rows[0]);
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
+
     }
 
-    async save(): Promise<User> {
-        return 
+    async getUserByUniqueKey(key: string, val: string): Promise<User> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `${this.baseQuery} where au.${key} = $1`;
+            let rs = await client.query(sql, [val]);
+            return mapUserResultSet(rs.rows[0]);
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+        
+    
     }
 
-    async update(): Promise<boolean> {
-         return
+    async getUserByCredentials(un: string, pw: string) {
+        
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `${this.baseQuery} where au.username = $1 and au.password = $2`;
+            let rs = await client.query(sql, [un, pw]);
+            return mapUserResultSet(rs.rows[0]);
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
     }
 
-    async deleteById(): Promise<boolean> {
-        return
+    async save(newUser: User): Promise<User> {
+            
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+
+            // WIP: hacky fix since we need to make two DB calls
+            let roleId = (await client.query('select id from user_roles where name = $1', [newUser.role])).rows[0].id;
+            
+            let sql = `
+                insert into app_users (username, password, first_name, last_name, email, role_id) 
+                values ($1, $2, $3, $4, $5, $6) returning id
+            `;
+
+            let rs = await client.query(sql, [newUser.username, newUser.password, newUser.firstName, newUser.lastName, newUser.email, roleId]);
+            
+            newUser.id = rs.rows[0].id;
+            
+            return newUser;
+
+        } catch (e) {
+            console.log(e);
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
     }
+
+    async update(updatedUser: User): Promise<boolean> {
+        
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = ``;
+            let rs = await client.query(sql, []);
+            return true;
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
+    }
+
+    async deleteById(id: number): Promise<boolean> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = ``;
+            let rs = await client.query(sql, []);
+            return true;
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+
+    }
+
 }
---
+++ src/routers/user-router.ts
@@ -0,0 +1,30 @@
+import url from 'url';
+import express from 'express';
+import AppConfig from '../config/app';
+import { isEmptyObject } from '../util/validator';
+import { ParsedUrlQuery } from 'querystring';
+import { adminGuard } from '../middleware/auth-middleware';
+
+export const UserRouter = express.Router();
+
+const userService = AppConfig.userService;
+
+UserRouter.get('', adminGuard, async (req, resp) => {
+
+    try {
+
+        let reqURL = url.parse(req.url, true);
+
+        if(!isEmptyObject<ParsedUrlQuery>(reqURL.query)) {
+            let payload = await userService.getUserByUniqueKey({...reqURL.query});
+            resp.status(200).json(payload);
+        } else {
+            let payload = await userService.getAllUsers();
+            resp.status(200).json(payload);
+        }
+
+    } catch (e) {
+        resp.status(e.statusCode).json(e);
+    }
+
+});
